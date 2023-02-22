22cebc1163cd4fb2fa00d7cabe9d3956f47b5ab7
KelseyDang2002 <kelseydang813@gmail.com>
2023-02-21 21:06:51 -0800

    a1

--- p1a1.py
+++ p1a1.py
@@ -13,187 +13,77 @@ c = len(city_distance_list)
 fuel_list = []
 f = len(fuel_list)
 
+# function for calculating preferred starting city
+# def path(city_distance_list, fuel_list, miles_per_gallon, c, f):
+#
+#     start = 0
+#     deficit = 0 # when value of capacity is negative it is stored in deficit
+#     capacity = 0
+#
+#     for i in range(f):
+#         capacity += (miles_per_gallon * fuel_list[i]) - city_distance_list[i + 1]
+#         if (capacity < 0): # if capacity is negative then increment start
+#             start = i + 1
+#             deficit += capacity
+#             capacity = 0
+#
+#     if (capacity + deficit >= 0):
+#         return start
+#     else:
+#         return -1
+
+# def path(city_distance_list, fuel_list, miles_per_gallon, c, f):
+#
+#     for (i, j) in zip(fuel_list, city_distance_list):
+#         start_fuel = i
+#         start_city = j
+#
+#         miles = (miles_per_gallon * fuel_list[i])
+#         next_city_distance = city_distance_list[i + 1]
+#         miles_left = miles - next_city_distance
+#
+#         return miles_left
+#
+#         if (miles_left < 0):
+#             start_fuel = i + 1
+#             start_city = j + 1
+#
+#         if (miles_left >= 0):
+#             return start_fuel, start_city
+#         else:
+#             return -1
+
+def path(city_distance_list, fuel_list, miles_per_gallon, c, f):
+
+# it literally just if ( gas tank < 0) { start = i + 1/increment the index/ curr fuel = 0;
+# /reset the cars gas/ to reset the loop then
+# if(totalFuel < 0)
+#     return -1
+# else
+#     return start
+
+    start = 0
+
+    miles = (miles_per_gallon * fuel_list[i])
+    next_city_distance = city_distance_list[i + 1]
+    miles_left = miles - next_city_distance
+
+    if (miles >= 0):
+        return start
+    else:
+        return -1
+
+# user enters city distances, fuel, and mpg
 city_distance_list = [int(i) for i in input("\tEnter city distances: ").split()]
-
 fuel_list = [int(i) for i in input("\tEnter fuel for each city: ").split()]
-
 miles_per_gallon = int(input("\tEnter miles per gallon: "))
 
 print("\ndistance: ", city_distance_list)
 print("fuel: ", fuel_list)
 print("mpg: ", miles_per_gallon)
 
-def path(city_distance_list, fuel_list, miles_per_gallon, c, f):
-    start = 0
-    deficit = 0
-    capacity = 0
-
-    for i in range(f):
-        capacity += (miles_per_gallon * fuel_list[i]) - city_distance_list[i + 1]
-        if (capacity < 0):
-            start = i + 1
-            deficit += capacity
-            capacity = 0
-
-    if (capacity + deficit >= 0):
-        return start
-    else:
-        return -1
-
-start = path(city_distance_list, fuel_list, miles_per_gallon, c, f)
-if (start == -1):
+result = path(city_distance_list, fuel_list, miles_per_gallon, c, f)
+if (result == -1):
     print("\nNo possible solution")
 else:
-    print("\nPreferred starting city = ", start)
-
-# miles = (miles_per_gallon * fuel_gallons[ind])
-# next_city_fuel = (miles_per_gallon * fuel_gallons[ind + 1])
-
-# i = number of cities
-# num_cities = int(input("Enter the number of cities: "))
-
-# for i in range(1, num_cities + 1):
-    # distance = int(input("Enter distance: "))
-    # city_distance_list.append(distance)
-
-# print("city_distance_list = ", city_distance_list)
-# print("Shortest distance: ", min(city_distance_list))
-
-# best case: minimum distance of next city + maximum next city gallon?
-'''
-# Method 1 to traverse: circular linked list
-# structure for a Node
-class Node:
-    # constructor to create a new node
-    def __init__(self, data):
-        self.data = data
-        self.next = None
-
-class CircularLinkedList:
-    # constructor to create an empty circular linked list
-    def __init__(self):
-        self.head = None
-
-    # function to insert a node at the beginning of a circular linked list
-    def push(self, data):
-        ptr1 = Node(data)
-        temp = self.head
-
-        ptr1.next = self.head
-
-        # if linked list is not None then set the next of last node
-        if self.head is not None:
-            while(temp.next != self.head):
-                temp = temp.next
-            temp.next = ptr1
-
-        else:
-            ptr1.next = ptr1 # for the first node
-
-        self.head = ptr1
-
-    # function to print nodes in a given circular linked list
-    def printList(self):
-        temp = self.head
-        if self.head is not None:
-            while(True):
-                print(temp.data, end = "\n")
-                time.sleep(1)
-                temp = temp.next
-                if(temp == self.head):
-                    break
-
-# initialize list as empty
-cllist = CircularLinkedList()
-
-# created linked list will be 5->25->15->10->15
-print("Order of cities according to city_distance_list:\n")
-cllist.push(15)
-cllist.push(10)
-cllist.push(15)
-cllist.push(25)
-cllist.push(5)
-
-cllist.printList()
-
-# Method 2 to traverse: cycle()
-def cycle(iterable):
-    saved = []
-    for element in iterable:
-        yield element
-        saved.append(element)
-    while saved:
-        for element in saved:
-            yield element
-
-# loops forever though
-# pool = cycle(city_distance_list)
-# for i in pool:
-#     print(i)
-
-# Method 3 to traverse: circular array
-# takes O(n) time but takes O(n) extra space
-def prints(a, n, ind):
-    # create auxillary array of twice size
-    b = [None] * 2 * n
-    i = 0
-
-    # copy a[] and b[] two times
-    while i < n:
-        b[i] = b[n + i] = a[i]
-        i = i + 1
-
-    i = ind
-
-    # print from ind-th index to (n+i)th index
-    while i < n + ind:
-        print(b[i], end = "\n")
-        time.sleep(1)
-        i = i + 1
-
-# Method 3.1 to traverse: circular array
-# circular array without using extra memory space
-# takes O(n) time and O(1) extra space
-def traverse(a, n, ind):
-    i = ind
-
-    while i < n + ind:
-        print(a[i % n], end = "\n")
-        time.sleep(1)
-        i = i + 1
-
-# Driver Code
-a = [5, 25, 15, 10, 15]
-n = len(a)
-miles = (miles_per_gallon * fuel_gallons[ind])
-next_city_fuel = (miles_per_gallon * fuel_gallons[ind + 1])
-ind = int(input("Enter starting city(0-4): "))
-print("Order of cities:\n")
-#prints(a, n, ind)
-for ind in a:
-    if miles + next_city_fuel >= a[ind + 1]:
-        traverse(a, n, ind)
-
-    else:
-        print("Not enough miles to travel to next city.\n")
-        print("Miles left: ", miles + next_city_fuel)
-'''
-# Program Output
-# Algorithm 1
-#
-# Enter mpg (positive integer): 10
-# Order of cities according to city_distance_list:
-
-# 5
-# 25
-# 15
-# 10
-# 15
-# Enter starting city(0-4): 3
-# Order of cities:
-
-# 10
-# 15
-# 5
-# 25
-# 15
+    print("\nPreferred starting city = ", result)
