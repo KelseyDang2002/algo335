47d9b7830109e4c79e7118de3eb89358e4ac98d7
KelseyDang2002 <kelseydang813@gmail.com>
2023-02-22 20:02:44 -0800

    a1 done

--- README.txt
+++ README.txt
@@ -1,12 +1,15 @@
 Name: Kelsey Dang
 Email: kdangdo2002@csu.fullerton.edu
 Project 1
-
+--------------------------------------------------------------------------------
 Algorithm 1:
 
-Enter city distances separated by spaces followed by <enter>
-Enter each city's fuel separated by spaces followed by <enter>
+Enter 5 city distances separated by spaces followed by <enter>
+Enter the 5 city's fuel separated by spaces followed by <enter>
 Enter miles per gallon of car followed by <enter>
 *All inputs are positive integers*
 
+Time efficiency of Algorithm 1: O(n) = 1 + 1 + 1 + O(5n + 4 + 6) + 3
+Method Used: Step Counting
+--------------------------------------------------------------------------------
 Algorithm 2:
--- p1a1.py
+++ p1a1.py
@@ -3,87 +3,44 @@
 # Filename: p1a1.py
 # Project Name: Project 1 Algorithm 1
 
-import time
-import itertools
-
 print("Algorithm 1\n")
 
+# define city distance array and fuel array
 city_distance_list = []
-c = len(city_distance_list)
 fuel_list = []
-f = len(fuel_list)
 
 # function for calculating preferred starting city
-# def path(city_distance_list, fuel_list, miles_per_gallon, c, f):
-#
-#     start = 0
-#     deficit = 0 # when value of capacity is negative it is stored in deficit
-#     capacity = 0
-#
-#     for i in range(f):
-#         capacity += (miles_per_gallon * fuel_list[i]) - city_distance_list[i + 1]
-#         if (capacity < 0): # if capacity is negative then increment start
-#             start = i + 1
-#             deficit += capacity
-#             capacity = 0
-#
-#     if (capacity + deficit >= 0):
-#         return start
-#     else:
-#         return -1
+def path(city_distance_list, fuel_list, miles_per_gallon):
 
-# def path(city_distance_list, fuel_list, miles_per_gallon, c, f):
-#
-#     for (i, j) in zip(fuel_list, city_distance_list):
-#         start_fuel = i
-#         start_city = j
-#
-#         miles = (miles_per_gallon * fuel_list[i])
-#         next_city_distance = city_distance_list[i + 1]
-#         miles_left = miles - next_city_distance
-#
-#         return miles_left
-#
-#         if (miles_left < 0):
-#             start_fuel = i + 1
-#             start_city = j + 1
-#
-#         if (miles_left >= 0):
-#             return start_fuel, start_city
-#         else:
-#             return -1
+    start = 0 # starting index initially at 0
+    deficit = 0 # when value of capacity is negative it is stored in deficit
+    capacity = 0 # capacity intially at 0
 
-def path(city_distance_list, fuel_list, miles_per_gallon, c, f):
+    for i in range(5):
+        capacity += (miles_per_gallon * fuel_list[i]) - city_distance_list[i] # formula
+        if (capacity < 0): # if capacity is negative then increment to change starting city
+            start = i + 1
+            deficit += capacity # calculate deficit
+            capacity = 0 # reset car fuel to 0 after changing city
 
-# it literally just if ( gas tank < 0) { start = i + 1/increment the index/ curr fuel = 0;
-# /reset the cars gas/ to reset the loop then
-# if(totalFuel < 0)
-#     return -1
-# else
-#     return start
-
-    start = 0
-
-    miles = (miles_per_gallon * fuel_list[i])
-    next_city_distance = city_distance_list[i + 1]
-    miles_left = miles - next_city_distance
-
-    if (miles >= 0):
+    if (capacity + deficit >= 0): # if deficit is >= 0 then traverse to next city
         return start
     else:
-        return -1
+        return -1 # if there is no solution
 
 # user enters city distances, fuel, and mpg
 city_distance_list = [int(i) for i in input("\tEnter city distances: ").split()]
 fuel_list = [int(i) for i in input("\tEnter fuel for each city: ").split()]
 miles_per_gallon = int(input("\tEnter miles per gallon: "))
 
+# displays the city_distance_list array, the fuel_list array, and the mpg entered
 print("\ndistance: ", city_distance_list)
 print("fuel: ", fuel_list)
 print("mpg: ", miles_per_gallon)
 
-result = path(city_distance_list, fuel_list, miles_per_gallon, c, f)
+# function call
+result = path(city_distance_list, fuel_list, miles_per_gallon)
 if (result == -1):
-    print("\nNo possible solution")
+    print("\nNo possible solution") # if there isn't a solution
 else:
-    print("\nPreferred starting city = ", result)
+    print("\nPreferred starting city = ", result) # prints the solution
