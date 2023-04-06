73b0cce7b3d830d6e1357b7eac5ee5778fec3304
KelseyDang2002 <kelseydang813@gmail.com>
2023-04-05 20:40:26 -0700

    p3 WIP

--- project3/dynamic.py
+++ project3/dynamic.py
@@ -4,3 +4,30 @@
 # Project 3 Dynamic Programming Algorithm
 
 print("Algorithm 2: Dynamic Programming\n")
+
+# input: a r x c matrix F, where each cell is either passable or impassable (X);
+# and F[0][0] = .
+#
+# output: the number of different paths starting at (0,0) and ending at (r-1,c-1);
+# where each step is either start, right move, or down move; and does not visit any X cell
+#
+# soccer_dynamic(F):
+#     # corner case: initial cell is impassable
+#     if F[0][0] == X:
+#         return 0
+#     A = new r x c matrix initialized to zeroes
+#     # base case
+#     A[0][0] = 1
+#     # general cases
+#     for i from 0 to r-1 inclusive:
+#         for i from 0 to c-1 inclusive:
+#             if F[i][j] == X:
+#                 A[i][j] = 0
+#                 continue
+#             above = from_left = 0
+#             if i > 0 and F[i-1][j] == .:
+#                 above = A[i-1][j]
+#             if j > 0 and F[i][j-1] == .:
+#                 left = A[i][j-1]
+#             A[i][j] += above + left
+#     return A[r-1][c-1]
--- project3/exhaustive.py
+++ project3/exhaustive.py
@@ -4,3 +4,26 @@
 # Project 3 Exhaustive Search Algorithm
 
 print("Algorithm 1: Exhaustive Search\n")
+
+# input: a r x c matrix F, where each cell is either passable or impassable (X);
+# and F[0][0] = .
+#
+# output: the number of different paths starting at (0,0) and ending at (r-1,c-1);
+# where each step is either start, right move, or down move; and does not visit any X cell
+#
+# soccer_exhaustive(F):
+#     len = r + c - 2
+#     counter = 0
+#     for bits from 0 to ((2^len) - 1) inclusive:
+#         candidate = empty list of moves
+#         for k from 0 to (len - 1) inclusive:
+#             bit = (bits >> k) & 1
+#             if bit == 1:
+#                 candidate.add(right)
+#             else:
+#                 candidate.add(down)
+#         if start or end location blocked:
+#             return 0
+#         if candidate stays inside grid, never crosses an X cell, and ends at (r-1,c-1):
+#             counter++
+#     return counter
