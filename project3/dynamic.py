b4a11e001bb9f33eb3e53a93f918bf6b814f21d4
KelseyDang2002 <kelseydang813@gmail.com>
2023-04-21 11:33:38 -0700

    exh update

--- project3/dynamic.py
+++ project3/dynamic.py
@@ -10,26 +10,33 @@ print("Algorithm 2: Dynamic Programming\n")
 r = int(input("\tEnter number of rows: ")) # row input
 c = int(input("\tEnter number or columns: ")) # column input
 
-matrix = [] # initialize matrix
+matrix = np.zeros((r, c)) # initialize matrix
 
-print("\nCreate", r, "x", c, "matrix by entering . or x")
-print("for each cell separated by white spaces:\n")
+player_positions = [(0,6),(0,8),(1,0),(2,3),(2,7),(3,2),(3,7),(4,1),(4,6),(5,4),(6,2),(6,8)]
 
-for i in range(r): # loop rows
-    arr = list(input().split()) # input . or x for cells of matrix row by row
-    matrix.append(arr) # append to arr (row-line for matrix)
+index = np.r_[player_positions].T
+matrix[index[0], index[1]] = 1
 
 print("\nMatrix: ")
-
-for i in range(r): # loop rows
-    for j in range(c): # loop columns
-        print(matrix[i][j], end = " ") # print each cell of matrix
-    print("") # print empty space
+print(matrix)
+# print("\nCreate", r, "x", c, "matrix by entering . or x")
+# print("for each cell separated by white spaces:\n")
+#
+# for i in range(r): # loop rows
+#     arr = list(input().split()) # input . or x for cells of matrix row by row
+#     matrix.append(arr) # append to arr (row-line for matrix)
+#
+# print("\nMatrix: ")
+#
+# for i in range(r): # loop rows
+#     for j in range(c): # loop columns
+#         print(matrix[i][j], end = " ") # print each cell of matrix
+#     print("") # print empty space
 
 # function, parameters: matrix, number of rows, number of columns
 def soccer_dynamic(matrix, r, c):
     # corner case: initial cell is impassable
-    if matrix[0][0] == 'x':
+    if matrix[0][0] == 1:
         return 0
     A = np.zeros((r, c)) # new r x c matrix initialized to zeroes
     # base case
@@ -37,13 +44,13 @@ def soccer_dynamic(matrix, r, c):
     # general cases
     for i in range(r): # loop rows
         for j in range(c): # loop columns
-            if matrix[i][j] == 'x': # if position is impassable
+            if matrix[i][j] == 1: # if position is impassable
                 A[i][j] = 0 # position in A matrix = 0
                 continue
             above = left = 0 # set above and left = 0
-            if i > 0 and matrix[i - 1][j] == '.': # if down position is passable
+            if i > 0 and matrix[i - 1][j] == 0: # if down position is passable
                 above = A[i - 1][j] # valid down move, update A
-            if j > 0 and matrix[i][j - 1] == '.': # if right position is passable
+            if j > 0 and matrix[i][j - 1] == 0: # if right position is passable
                 left = A[i][j - 1] # valid right move, update A
             A[i][j] += above + left # add position to A
     return print("\nPaths found: ", A[r - 1][c - 1]) # return number of paths found
--- project3/exhaustive.py
+++ project3/exhaustive.py
@@ -3,44 +3,108 @@
 # Filename: exhaustive.py
 # Project 3 Exhaustive Search Algorithm
 
-import bitstring
-
 print("Algorithm 1: Exhaustive Search\n")
+#
+# r = int(input("\tEnter number of rows: ")) # row input
+# c = int(input("\tEnter number or columns: ")) # column input
+#
+# matrix = [] # initialize matrix
+#
+# print("\nCreate", r, "x", c, "matrix by entering . or x")
+# print("for each cell separated by white spaces:\n")
+#
+# for i in range(r): # loop rows
+#     arr = list(input().split()) # input . or x for cells of matrix row by row
+#     matrix.append(arr) # append to arr (row-line for matrix)
+#
+# print("\nMatrix: ")
+#
+# for i in range(r): # loop rows
+#     for j in range(c): # loop columns
+#         print(matrix[i][j], end = " ") # print each cell of matrix
+#     print("") # print empty space
+#
+# # exhaustive search function
+# def soccer_exhaustive(matrix, i, j, r, c):
+#     length = r + c - 2
+#     counter = 0
+#     for bits in range(0, 2**length):
+#         candidate = []
+#         for k in range(0, length):
+#             bit = (bits >> k) & 1
+#             if bit == 1:
+#                 candidate.append(1)
+#             else:
+#                 candidate.append(0)
+#         if  i == r-1 and j == c-1:
+#             counter += 1
+#     return print(counter)
+#
+# print("\nPaths found: ", end = "")
+# soccer_exhaustive(matrix, i, j, r, c) # function call
 
-r = int(input("\tEnter number of rows: ")) # row input
-c = int(input("\tEnter number or columns: ")) # column input
+# Board Class (for easy debug)
+class Board:
+    def __init__(self, r, c):
+        self.rows = r
+        self.columns = c
+        self.board = self.empty_board_fill(self.rows, self.columns)
 
-matrix = [] # initialize matrix
+    def empty_board_fill(self, r, c):
+        field = [[0 for x in range(c)] for y in range(r)]
+        return field
 
-print("\nCreate", r, "x", c, "matrix by entering . or x")
-print("for each cell separated by white spaces:\n")
+    def player_positions(self, *args):
+        for arg in args:
+            self.board[arg[0]][arg[1]] = 1
 
-for i in range(r): # loop rows
-    arr = list(input().split()) # input . or x for cells of matrix row by row
-    matrix.append(arr) # append to arr (row-line for matrix)
+    def display(self):
+        for i in range(self.rows):
+            print(self.board[i])
 
-print("\nMatrix: ")
+def test_case():
+    test = Board(8, 9)
+    test.player_positions((0,6),(0,8),(1,0),(2,3),(2,7),(3,2),(3,7),(4,1),(4,6),(5,4),(6,2),(6,8))
+    test.display()
+    return test
 
-for i in range(r): # loop rows
-    for j in range(c): # loop columns
-        print(matrix[i][j], end = " ") # print each cell of matrix
-    print("") # print empty space
+# Parameters: candidate is a string od 1s and 0s of length (r+c-2),
+# field = 2-D array representing the players as 1s and empty spaces as 0s
+def validate_solution(candidate, field):
+    # run through the binary string where 0 = right(+1 row), 1 = down(+1 column)
+    pos = [0,0]
+    for bit in candidate:
+        # Check if a move right is valid
+        if bit == "0":
+            if pos[0]+1 < field.rows:               # inside board check
+                pos[0] += 1                         # move right one spot
+            if field.board[pos[0]][pos[1]] == 1:    # player collision check
+                return False
+        # Check if a move down is valid
+        else:
+            if pos[1]+1 < field.columns:            # inside board checkS
+                pos[1] += 1                         # move down one spot
+            if field.board[pos[0]][pos[1]] == 1:    # player collision check
+                return False
+    # Check if final position is [r-1][c-1]
+    if pos != [field.rows - 1, field.columns - 1]:
+        return False
+    return True
 
-# exhaustive search function
-def soccer_exhaustive(matrix, i, j, r, c):
-    length = r + c - 2
+def soccer_exhaustive():
+    field = test_case()
+    size = field.rows + field.columns - 2
     counter = 0
-    for bits in range(0, 2**length):
-        candidate = []
-        for k in range(0, length):
-            bit = (bits >> k) & 1
-            if bit == 1:
-                candidate.append(1)
-            else:
-                candidate.append(0)
-        if  i == r-1 and j == c-1:
-            counter += 1
-    return print(counter)
 
-print("\nPaths found: ", end = "")
-soccer_exhaustive(matrix, i, j, r, c) # function call
+    # Generate Candidate Solutions (binary strings of length (r+c-2))
+    candidate_solutions = list()
+    for i in range(2**size, 2**(size+1)):
+        candidate = (bin(i)[3:])
+        if (validate_solution(candidate, field) == True):
+            counter += 1
+
+    return counter
+
+# Driver
+final_total = soccer_exhaustive()
+print(final_total)
